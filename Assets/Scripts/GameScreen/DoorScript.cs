5f89b08adba4a1cbe74e17c47a6f0b445932449b
Alexx915 <168471081+Alexx915@users.noreply.github.com>
2025-03-31 15:27:14 +0200

    Update Doors

--- Assets/Scripts/GameScreen/DoorScript.cs
+++ Assets/Scripts/GameScreen/DoorScript.cs
@@ -4,24 +4,50 @@ public class DoorScript : MonoBehaviour
 {
     public GameObject puertaR;
     public GameObject puertaL;
+    public bool rotate;
+
+    private int Rx;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<MovementScript>() != null)
         {
-            puertaR.transform.rotation = new Quaternion(0, -90, 0, -90);
-            puertaL.transform.rotation = new Quaternion(0, 90, 0, -90);
+            if (puertaR != null)
+            {
+                if (puertaR.transform.rotation.x != 0)
+                {
+                    Rx = 180;
+                }
+                else
+                {
+                    Rx = 0;
+                }
+                //puertaR.transform.rotation = new Quaternion(Rx, 0, 0, 0);
+                puertaR.transform.Rotate(new Vector3(0,90f,0));
+            }
+            if (puertaL != null)
+            {
+                //puertaL.transform.rotation = new Quaternion(0, 0, 0, 0);
+                puertaL.transform.Rotate(new Vector3(0, -90f, 0));
+            }
 
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        
+        
         if (other.gameObject.GetComponent<MovementScript>() != null)
         {
-            puertaR.transform.rotation = new Quaternion(0, 0, 0, 0);
-            puertaL.transform.rotation = new Quaternion(0, 0, 0, 0);
-            Debug.Log("CERRAR");
+            if (puertaR != null)
+            {
+                puertaR.transform.Rotate(new Vector3(0, -90, 0));
+            }
+            if (puertaL != null)
+            {
+                puertaL.transform.Rotate(new Vector3(0, 90, 0));
+            }
         }
     }
 }
