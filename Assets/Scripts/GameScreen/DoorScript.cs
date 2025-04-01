6da636dcf8c2f54a558b6ab9d49ce0cceb0e773d
Alexx915 <168471081+Alexx915@users.noreply.github.com>
2025-04-01 10:05:39 +0200

    All Doors Working

--- Assets/Scripts/GameScreen/DoorScript.cs
+++ Assets/Scripts/GameScreen/DoorScript.cs
@@ -4,33 +4,45 @@ public class DoorScript : MonoBehaviour
 {
     public GameObject puertaR;
     public GameObject puertaL;
-    public bool rotate;
 
-    private int Rx;
+    private bool cerca;
+    private bool abierto;
+    private void Update()
+    {
+        if (cerca && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!abierto)
+            {
+                if (puertaR != null)
+                {
+                    puertaR.transform.Rotate(new Vector3(0, 90f, 0));
+                }
+                if (puertaL != null)
+                {
+                    puertaL.transform.Rotate(new Vector3(0, -90f, 0));
+                }
+                abierto = true;
+            }
+            else 
+            {
+                if (puertaR != null)
+                {
+                    puertaR.transform.Rotate(new Vector3(0, -90, 0));
+                }
+                if (puertaL != null)
+                {
+                    puertaL.transform.Rotate(new Vector3(0, 90, 0));
+                }
+                abierto = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<MovementScript>() != null)
         {
-            if (puertaR != null)
-            {
-                if (puertaR.transform.rotation.x != 0)
-                {
-                    Rx = 180;
-                }
-                else
-                {
-                    Rx = 0;
-                }
-                //puertaR.transform.rotation = new Quaternion(Rx, 0, 0, 0);
-                puertaR.transform.Rotate(new Vector3(0,90f,0));
-            }
-            if (puertaL != null)
-            {
-                //puertaL.transform.rotation = new Quaternion(0, 0, 0, 0);
-                puertaL.transform.Rotate(new Vector3(0, -90f, 0));
-            }
-
+            cerca = true;
         }
     }
 
@@ -40,14 +52,7 @@ public class DoorScript : MonoBehaviour
         
         if (other.gameObject.GetComponent<MovementScript>() != null)
         {
-            if (puertaR != null)
-            {
-                puertaR.transform.Rotate(new Vector3(0, -90, 0));
-            }
-            if (puertaL != null)
-            {
-                puertaL.transform.Rotate(new Vector3(0, 90, 0));
-            }
+            cerca = false;
         }
     }
 }
